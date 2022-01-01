658b7cd707ef1fdf07013f4c8f9b6e02866b26e6
stephen0135636 <60317985+stephen0135636@users.noreply.github.com>
2022-01-01 20:16:21 +0100

    commit made

---
+++ public/js/script.js
@@ -0,0 +1,74 @@
+$(document).ready(function(){
+  $(window).scroll(function(){
+      // sticky navbar on scroll script
+      if(this.scrollY > 20){
+          $('.navbar').addClass("sticky");
+      }else{
+          $('.navbar').removeClass("sticky");
+      }
+      
+      // scroll-up button show/hide script
+      if(this.scrollY > 500){
+          $('.scroll-up-btn').addClass("show");
+      }else{
+          $('.scroll-up-btn').removeClass("show");
+      }
+  });
+
+  // slide-up script
+  $('.scroll-up-btn').click(function(){
+      $('html').animate({scrollTop: 0});
+      // removing smooth scroll on slide-up button click
+      $('html').css("scrollBehavior", "auto");
+  });
+
+  $('.navbar .menu li a').click(function(){
+      // applying again smooth scroll on menu items click
+      $('html').css("scrollBehavior", "smooth");
+  });
+
+  // toggle menu/navbar script
+  $('.menu-btn').click(function(){
+      $('.navbar .menu').toggleClass("active");
+      $('.menu-btn i').toggleClass("active");
+  });
+});
+
+
+let count = 0;
+//if add to cart btn clicked
+$('.cart-btn').on('click', function (){
+  let cart = $('.cart-nav');
+  // find the img of that card which button is clicked by user
+  let imgtodrag = $(this).parent('.buttons').parent('.content').parent('.card').find("img").eq(0);
+  if (imgtodrag) {
+    // duplicate the img
+    var imgclone = imgtodrag.clone().offset({
+      top: imgtodrag.offset().top,
+      left: imgtodrag.offset().left
+    }).css({
+      'opacity': '0.8',
+      'position': 'absolute',
+      'height': '150px',
+      'width': '150px',
+      'z-index': '100'
+    }).appendTo($('body')).animate({
+      'top': cart.offset().top + 20,
+      'left': cart.offset().left + 30,
+      'width': 75,
+      'height': 75
+    }, 1000, 'easeInOutExpo');
+
+    setTimeout(function(){
+      count++;
+      $(".cart-nav .item-count").text(count);
+    }, 1500);
+
+    imgclone.animate({
+      'width': 0,
+      'height': 0
+    }, function(){
+      $(this).detach()
+    });
+  }
+});
